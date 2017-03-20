2f232711b137b20281b6b899f25d134d2f7497bf
mahesh chand <shivangi.gupta@knoldus.in>
2017-03-20 10:58:36 +0530

    changed bookmyshow

--- src/main/scala/BookMyShow.scala
+++ src/main/scala/BookMyShow.scala
@@ -5,67 +5,40 @@ import com.typesafe.config.ConfigFactory
 
 
 class BookMyShow extends Actor{
-
-
-  var flag = false
-  var isFirstTimeVisit = true
+  val maxSeat = 5
+  var totalNoOfSeats = maxSeat
   def receive = {
-    case request if(request == "Book Seat 1") => {
-      if(flag == false) {
-
-        flag = true
-        println(s"Thanks for Booking")
+    case request:String if(request == "Book Seat 1")   => if(totalNoOfSeats != 0)
+      {
+        totalNoOfSeats -= 1
+        println("Seat is successfully booked")
       }
       else
-        println("Seat is already booked")
-    }
+      println("Show is Full")
 
-    case request if(request == "Cancel Seat 1") => {
-      if(flag == true) {
-        flag = false
+    case request: String if(request == "Cancel Seat 1") => if(totalNoOfSeats == maxSeat)
+      println("Show is Empty")
+
+      else if(totalNoOfSeats < 5 )
+      {
+        totalNoOfSeats += 1
         println("Seat is successfully cancelled")
+
       }
 
-      else {
-        if(isFirstTimeVisit == true) {
-          isFirstTimeVisit = false
-          println("You have not booked any seat so you cant cancel")
-        }
-        else
-        println("Seat is already cancelled")
-      }
-
-    }
 
   }
 
-}
+  }
 
 object BookMyShow extends App {
 
+  val system = ActorSystem("BookMyShow")
+  val actor = system.actorOf(Props[BookMyShow])
+  actor ! "Cancel Seat 1"
+  actor ! "Book Seat 1"
+  actor ! "Book Seat 1"
+  actor ! "Cancel Seat 1"
 
-  val config = ConfigFactory.parseString(
-    """
-      |akka.actor.deployment {
-      | /poolRouter {
-      |   router = round-robin-pool
-      |   nr-of-instances = 5
-      | }
-      |}
-    """.stripMargin
-  )
-  val system = ActorSystem("RouterSystem", config)
-  val actor = system.actorOf(FromConfig.props(Props[BookMyShow]), "poolRouter")
-
-
-  actor ! "Book Seat 1"
-  actor ! "Book Seat 1"
-  actor ! "Book Seat 1"
-  actor ! "Cancel Seat 1"
-  actor ! "Cancel Seat 1"
-  actor ! "Book Seat 1"
-  actor ! "Cancel Seat 1"
-  actor ! "Cancel Seat 1"
-  actor ! "Cancel Seat 1"
 
 }
